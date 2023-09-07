8000b0411d350ddf6c5d7bef5382f553941d3e6d
monsieur-monet <mail@tscheer.dev>
2023-09-07 19:45:58 +0200

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,17 @@
 <script>
-    import GridItem from "$lib/GridItem.svelte";
-    import Grid from "$lib/Grid.svelte";
-    import Text from "$lib/Text.svelte";
     import "../app.css";
     import Container from "../lib/Container.svelte";
     import Header from "../lib/Header.svelte";
+    import Spacing from "$lib/Spacing.svelte";
+    import Headline from "$lib/Headline.svelte";
+
+    const imgUrl = new URL("../assets/timo.jpg", import.meta.url).href;
 </script>
 
 <Container>
     <Header />
-    <Grid cols="3">
-        <GridItem span="2">
-            <Text>
-                Hey, ich bin Timo und Frontend Entwickler. Ich liebe neue
-                Technologien und bin immer auf der Suche nach neuen
-                Herausforderungen. Meine Leidenschaft ist es, komplexe Probleme
-                zu lösen und dabei die bestmögliche User Experience zu schaffen.
-                Ich bin ein Teamplayer und mag es, mit anderen
-                zusammenzuarbeiten. Ich bin ein sehr neugieriger Mensch und
-                lerne gerne neue Dinge. Ich bin sehr selbstkritisch und versuche
-                immer, mich zu verbessern. Ich bin ein sehr kreativer Mensch und
-                liebe es zu zeichnen.
-                <br /> <br />
-                Dieser dumme Text ist nur hier, um die Seite zu füllen. Ich habe
-                keine Ahnung, was ich sonst schreiben soll.
-            </Text>
-        </GridItem>
-        <GridItem span="1">Tolle Scheiße hier</GridItem>
-    </Grid>
+    <Spacing />
+    <Headline center="true" variant="h1" >Hey, hier entsteht eine tolle Website 🚀</Headline>
 </Container>
 
 <style lang="postcss">
