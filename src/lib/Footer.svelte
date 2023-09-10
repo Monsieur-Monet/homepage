952698e8cc5b2ca14116092dfa9c1f03682db682
timo.scheer <timo.scheer@bytediver.de>
2023-09-10 17:07:42 +0200

    Remove background from Footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,7 +1,6 @@
-<div class="bg-primary">
+
 <div class="container mx-auto px-4 h-10 pt-2">
-  <div class="text-white flex justify-center">
+  <div class="flex justify-center">
     Copyright 2023 - Timo Scheer
   </div>
-</div>
 </div>
