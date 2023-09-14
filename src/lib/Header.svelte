0d7f04654e11aec2480f0300e66053352dbf39d9
monsieur-monet <mail@tscheer.dev>
2023-09-14 21:59:25 +0200

    fixing header on content page

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,10 +1,20 @@
-<script>
+<script lang="ts">
   import Headline from "./Headline.svelte";
   import Navigation from "$lib/Navigation.svelte";
+  import { page } from "$app/stores";
+
+  let path: String;
+
+  function getPath(currentPath: string | String) {
+    path = currentPath;
+  }
+
+  $: getPath($page.url.pathname);
 </script>
 
 <div
   class="flex justify-between h-20 p-4 z-10 items-center top-0 w-full container mx-auto left-1/2 -translate-x-1/2 fixed"
+  class:bg-background={path !== "/"}
 >
   <Headline variant="h2">Timo Scheer</Headline>
   <div class="flex gap-4">
