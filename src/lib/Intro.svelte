5dbed23057691edd7b534a74d2a0f2a2d48f5abf
monsieur-monet <mail@tscheer.dev>
2023-09-14 18:22:28 +0200

    Updating scroll effect

--- src/lib/Intro.svelte
+++ src/lib/Intro.svelte
@@ -1,55 +1,45 @@
-<script>
+<script lang="ts">
     import Arrow from "$lib/Arrow.svelte";
     import { onMount } from "svelte";
-    import Container from "./Container.svelte";
+
+    export let afterIntro: HTMLDivElement;
 
     const imgUrlIntro = new URL("/src/assets/intro.jpg", import.meta.url).href;
     const imgUrlTimo = new URL("/src/assets/timo.jpg", import.meta.url).href;
 
-    function scrollIntoView({ target }) {
-        const el = document.querySelector(target.getAttribute("href"));
-        if (!el) return;
-        el.scrollIntoView({
-            behavior: "smooth",
-            block: "end",
-        });
-        console.log(el);
+    const greetings = ["Hi", "Salut", "Hey", "Hello"] as const;
+    let count = 0;
+    let headerPosY = 0;
+    let header: HTMLDivElement;
+    let greetingOpacity: 0 | 1 = 1;
+
+    function scrollIntoView() {
+        afterIntro.scrollIntoView({ behavior: "smooth" });
+    }
+
+    function scroll() {
+        headerPosY = window.scrollY;
     }
 
     onMount(() => {
-        let count = 0;
-        let wordsArray = ["Hi", "Salut", "Hey", "Hello"];
         setInterval(function () {
-            if (count < 4) {
-                document.getElementById("greeting").style.opacity = "0";
-                setTimeout(function () {
-                    document.getElementById("greeting").innerHTML =
-                        wordsArray.at(count);
-                    document.getElementById("greeting").style.opacity = "1";
-                    count++;
-
-                    if (count === 4) {
-                        count = 0;
-                    }
-                }, 500);
-            }
+            greetingOpacity = 0;
+            setTimeout(function () {
+                greetingOpacity = 1;
+                count = count === 3 ? 0 : count + 1;
+            }, 500);
         }, 5000);
     });
 </script>
 
-<div class="h-[100svh] w-full relative bg-black">
-    <div class="fixed top-1/4 md:top-1/2 -translate-y-1/2 w-full z-10">
-        <div class="container mx-auto px-4">
-            <div class="flex gap-4 md:gap-8 flex-col md:flex-row items-start">
-                <div
-                    id="greeting"
-                    class="text-white text-[5rem] md:text-[10rem] font-mono mix-blend-difference transition-opacity"
-                >
-                    Hello
-                </div>
-            </div>
-        </div>
-    </div>
+<svelte:window on:scroll={scroll} />
+<svelte:body class="bg-primary" />
+<div
+    class="h-[100svh] w-full bg-black relative"
+    style="opacity: {1 -
+        Math.min(headerPosY / header?.getBoundingClientRect().height, 1)}"
+    bind:this={header}
+>
     <img
         class="h-full w-full object-cover blur"
         src={imgUrlIntro}
@@ -63,6 +53,19 @@
     >
         <Arrow />
     </a>
+</div>
+
+<div class="fixed top-1/4 md:top-1/2 -translate-y-1/2 w-full z-10">
+    <div class="container mx-auto px-4">
+        <div class="flex gap-4 md:gap-8 flex-col md:flex-row items-start">
+            <div
+                style="opacity: {greetingOpacity};"
+                class="text-white text-[5rem] md:text-[10rem] font-mono mix-blend-difference transition-opacity"
+            >
+                {greetings[count]}
+            </div>
+        </div>
+    </div>
 </div>
 
 <!-- Photo by <a href="https://unsplash.com/@kirp?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">AndrewKliatskyi</a> on <ahref="https://unsplash.com/photos/ZYuB4x5hA1Q?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> -->
